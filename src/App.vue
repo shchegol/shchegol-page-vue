99c4119731ab085eb7ce15ac45ead2597ecce67c
Alexander Shchegol <zelenzoom@gmail.com>
2018-02-15 18:36:15 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" class="background">
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="scss">
+  @import 'assets/scss/app.scss';
+</style>
---
+++ src/components/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row card mt-3">
+    <div class="col">
+      <h1>Alexander Shchegol</h1>
+      <p>Frontend Developer</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index'
+  }
+</script>
