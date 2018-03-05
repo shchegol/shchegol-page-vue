119cff979274e7c34947f99899b845f491173f87
Alexander Shchegol <zelenzoom@gmail.com>
2018-03-05 21:08:00 +0300

    ### add blob

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container main-container">
     <div class="row justify-content-center">
-      <div class="col-8 card">
+      <div class="col-6">
         <header>
           <div class="row">
             <div class="col text-center">
@@ -16,15 +16,39 @@
         <router-view/>
       </div>
     </div>
+
+    <svg id="svg" viewBox="0 0 1000 1000">
+      <path id="blob" ref="blob"></path>
+    </svg>
   </div>
 </template>
 
 <script>
   import MainMenu from './components/MainMenu'
+  import { createBlob } from './assets/js/common'
 
   export default {
     components: {MainMenu},
-    name: 'App'
+    name: 'App',
+    data () {
+      return {
+        blob: null
+      }
+    },
+    mounted () {
+      let self = this
+
+      self.blob = createBlob({
+        element: self.$refs.blob,
+        numPoints: 5,
+        centerX: 500,
+        centerY: 500,
+        minRadius: 300,
+        maxRadius: 325,
+        minDuration: 5,
+        maxDuration: 10
+      })
+    }
   }
 </script>
 
