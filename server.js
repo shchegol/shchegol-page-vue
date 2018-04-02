576f34396c3f46e849d95aaaac3547e526219de1
Alexander Shchegol <zelenzoom@gmail.com>
2018-04-02 21:16:34 +0300

    heroku server configure

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const path = require('path')
-const port = process.env.PORT || 5000
-
-express()
-  .use('/static', express.static(path.join(`${__dirname}/dist/static`)))
-  .use('/', express.static(path.join(`${__dirname}/dist`)))
-  .get('/', (req, res) => res.sendFile(path.join(`${__dirname}/dist/index.html`)))
-  .listen(port, () => console.log(`Listening on ${port}`))
