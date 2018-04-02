27a5d46d04f702f170d958f12fdbcf5345773776
Alexander Shchegol <zelenzoom@gmail.com>
2018-04-02 19:05:06 +0300

    procfile added

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('*', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')))
+  .use('/static', express.static(path.join(`${__dirname}/dist/static`)))
+  .use('/', express.static(path.join(`${__dirname}/dist`)))
+  .get('*', (req, res) => res.sendFile(path.join(`${__dirname}/dist/index.html`)))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
